534bd3e1259b32ea947b2b3a78004705e3a9869c
devSachinkr <sachin67.dev@gmail.com>
2026-09-15 15:21:54 +0530

    Add Auth Feature with RBAC

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation";
+
+import { syncUserWithDatabase } from "@/actions/user/user.action";
+import { ROLE_ROUTES } from "@/lib/types/role.type";
+
+const page = async () => {
+  const result = await syncUserWithDatabase();
+
+  if (!result.success || !result.user) {
+    return redirect("/login");
+  }
+
+  redirect(ROLE_ROUTES[result.user.role]);
+};
+
+export default page;
---
+++ src/app/(main)/inspector/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {UserButton} from "@clerk/nextjs";
+const page = () => {
+  return (
+    <div>
+        Inspector SOON 
+        <UserButton/>
+    </div>
+  )
+}
+
+export default page
